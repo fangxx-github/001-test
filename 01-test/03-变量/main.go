5f009bd6462e37de648f9b7a4a6149464fd00ab8
xxfang <test@163.com>
2022-11-10 14:14:01 +0800

    frist4

---
+++ 01-test/03-变量/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var hello string = "hello,golang"
+	fmt.Println(hello)
+	var age int = 22
+	fmt.Println(age)
+	var fd float64 = 1.77777
+	fmt.Println(fd)
+}
