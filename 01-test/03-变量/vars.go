cf7bb01dccdfdd45ce22733ff34235c10bbfa528
xxfang <test@163.com>
2022-11-11 21:12:42 +0800

    ss

---
+++ 01-test/03-变量/vars.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string = "hello,golang"
+	var world = "world"
+	fmt.Println(hello, world)
+
+	xiaoshu := 3.1415926
+	fmt.Println(xiaoshu)
+
+	var f int
+	fmt.Println(f)
+
+}
---
+++ 01-test/04-常量/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const pi float64 = 3.1415926
+	fmt.Println(pi)
+	const a bool = true
+	fmt.Println(a)
+	const b int = 11
+	fmt.Println(b)
+	
+}
